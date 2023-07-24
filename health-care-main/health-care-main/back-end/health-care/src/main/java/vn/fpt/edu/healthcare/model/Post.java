0786311a9d361bc3f96540d0779d15a3aab48e8c
thanhndtdev <thanhndt.dev@gmail.com>
2023-07-24 15:27:50 +0700

    update
    update

---
+++ health-care-main/health-care-main/back-end/health-care/src/main/java/vn/fpt/edu/healthcare/model/Post.java
@@ -0,0 +1,22 @@
+package vn.fpt.edu.healthcare.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("post")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+    @Id
+    private String postId;
+    private String thumbnail;
+    private String title;
+    private String category;
+    private User author;
+    private String feature;
+    private int status;
+}
